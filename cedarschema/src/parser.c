30f7358e6aa68f94dac0aa98a662bfd962828415
Cathal <contact@cathal.dev>
2026-07-18 02:01:08 +0100

    chore: Release v0.2.0

--- cedarschema/src/parser.c
+++ cedarschema/src/parser.c
@@ -5297,7 +5297,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_cedarschema(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 1,
+      .minor_version = 2,
       .patch_version = 0,
     },
   };
